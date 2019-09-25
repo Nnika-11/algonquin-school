ab6f856c656fabbf5e6f426ea9ce6c528ee30a88
sher0201 <43050369+sher0201@users.noreply.github.com>
2019-09-25 15:21:46 -0400

    Add labs

---
+++ C#/Lab 1/BookStore.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class BookStore : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //get all books in the catalog.
+            List<Book> books = BookCatalogDataAccess.GetAllBooks();
+            foreach (Book book in books)
+            {
+                // Populate dropdown list selections 
+                ListItem item = new ListItem(book.Title, book.Id);
+                drpBookSelection.Items.Add(item);
+
+            }
+        }
+        ShoppingCart shoppingcart = null;
+        if (Session["shoppingcart"] == null)
+        {
+            shoppingcart = new ShoppingCart();
+            // add cart to the session
+            Session["shoppingcart"] = shoppingcart;
+        }
+        else
+        {
+            // retrieve cart from the session
+            shoppingcart = (ShoppingCart)Session["shoppingcart"];
+
+            foreach (BookOrder order in shoppingcart.BookOrders)
+            {
+                //Remove the book in the order from the dropdown list
+                ListItem item = new ListItem(order.Book.Title, order.Book.Id);
+                drpBookSelection.Items.Remove(item);
+            }
+        }
+
+        if (shoppingcart.NumOfItems == 0)
+            lblNumItems.Text = "empty";
+        else if (shoppingcart.NumOfItems == 1)
+            lblNumItems.Text = "1 item";
+        else
+            lblNumItems.Text = shoppingcart.NumOfItems.ToString() + " items";
+        
+    }
+    protected void drpBookSelection_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (drpBookSelection.SelectedValue != "-1")
+        {
+            string bookId = drpBookSelection.SelectedItem.Value;
+            Book selectedBook = BookCatalogDataAccess.GetBookById(bookId);
+
+            //Add selected book to the session
+            Session["book"] = selectedBook;
+            //Display the selected book's description and price 
+            lblDescription.Text = selectedBook.Description;
+            lblPrice.Text = "$" + selectedBook.Price;
+        }
+        else
+        {
+            //Set description and price to blank
+            lblDescription.Text = string.Empty;
+            lblPrice.Text = string.Empty;
+        }
+    }
+    protected void btnAddToCart_Click(object sender, EventArgs e)
+    {
+        if (drpBookSelection.SelectedValue != "-1" && Session["shoppingcart"] != null)
+        {
+            // Retrieve selected book from the session
+            Book selectedBook = (Book)Session["book"];
+
+            // get user entered quqntity
+            int quantity = int.Parse(txtQuantity.Text);
+            // Create a book order with selected book and quantity
+            BookOrder order = new BookOrder(selectedBook, quantity);
+            //Retrieve to cart from the session
+            ShoppingCart cart = (ShoppingCart)Session["shoppingcart"];
+            // Add book order to the shopping cart
+            cart.AddBookOrder(order);
+            // Remove the selected item from the dropdown list
+            drpBookSelection.Items.Remove(new ListItem(selectedBook.Title, selectedBook.Id));
+            // Set the dropdown list's selected value as "-1"
+            drpBookSelection.SelectedValue = "-1";
+            //Set the description to show title and quantity of the book user added to the shopping cart
+            lblDescription.Text = quantity+" copy of " + selectedBook.Title + " is added to the shopping cart";
+            //Update the number of items in shopping cart displayed next to the link to ShoppingCartView.aspx
+            lblNumItems.Text = cart.NumOfItems.ToString() + " items";
+        }
+    }
+}
---
+++ C#/Lab 1/ShoppingCartView.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ShoppingCartView : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["shoppingcart"] == null)
+        {
+            //Redirect the user to Bookstore.aspx page
+            Response.Redirect("BookStore.aspx");
+        }
+        else
+        {
+            //Retrieve shopping cart from the session
+            ShoppingCart shoppingcart = (ShoppingCart)Session["shoppingcart"];
+            // Call DisplayShoppingCart method to display shopping cart 
+            DisplayShoppingCart(shoppingcart);
+        }
+
+    }
+
+    protected void btnBack_Click(object sender, EventArgs e)
+    {
+        //Redirect the user to Bookstore.aspx page
+        Response.Redirect("BookStore.aspx");
+    }
+
+    protected void btnEmptyShoppingCart_Click(object sender, EventArgs e)
+    {
+        // Retrieve shopping cart from session.
+        //ShoppingCart shoppingcart = (ShoppingCart)Session["shoppingcart"];
+        //shoppingcart.BookOrders.Clear();
+
+        // Clear shopping cart
+        Session["shoppingcart"] = new ShoppingCart();
+        while (tblShoppingCart.Rows.Count > 1) tblShoppingCart.Rows.RemoveAt(1);
+
+        //Session.Contents.Remove("shoppingcart");
+        //Call DisplayShoppingCart method to display shopping cart 
+
+        DisplayShoppingCart((ShoppingCart)Session["shoppingcart"]);
+    }
+
+    private void DisplayShoppingCart(ShoppingCart cart)
+    {
+        
+        if (cart.IsEmpty)
+        {
+            TableRow lastRow = new TableRow();
+            TableCell lastRowCell = new TableCell();
+            lastRowCell.Text = "Your Shopping Cart is Empty";
+            lastRowCell.ForeColor = System.Drawing.Color.Red;
+            lastRowCell.ColumnSpan = 3;
+            lastRowCell.HorizontalAlign = HorizontalAlign.Center;
+            lastRow.Cells.Add(lastRowCell);
+            tblShoppingCart.Rows.Add(lastRow);
+        }
+        else
+        {
+            foreach (BookOrder order in cart.BookOrders)
+            {
+                TableRow row = new TableRow();
+
+                TableCell cell = new TableCell();
+                cell.Text = order.Book.Title;
+                row.Cells.Add(cell);
+
+                cell = new TableCell();
+                cell.Text = order.NumOfCopies.ToString();
+                row.Cells.Add(cell);
+
+                cell = new TableCell();
+                cell.Text = "$" + order.NumOfCopies * order.Book.Price;
+                row.Cells.Add(cell);
+
+                tblShoppingCart.Rows.Add(row);
+            }
+            TableRow lastRow = new TableRow();
+            TableCell lastRowCell = new TableCell();
+            lastRowCell.Text = "Total";
+            lastRowCell.ColumnSpan = 2;
+            lastRowCell.HorizontalAlign = HorizontalAlign.Right;
+            lastRow.Cells.Add(lastRowCell);
+
+            lastRowCell = new TableCell();
+            lastRowCell.Text = "$" + cart.TotalAmountPayable.ToString();
+            lastRow.Cells.Add(lastRowCell);
+
+            tblShoppingCart.Rows.Add(lastRow);
+        }
+    }
+}
