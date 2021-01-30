391c35dd78be60dedbb0d9f11f0622a3c75e1d15
Nnika-11 <sherr.veronika@gmail.com>
2021-01-30 10:59:41 +0300

    Lab2

---
+++ C#/lab2.2/Lab 2/RestaurantReview.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Xml.Serialization;
+
+
+public partial class RestaurantReview : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {   
+        
+        if (!IsPostBack)
+        {
+            //Use the names of the restaurants in the XML file  to populate the dropdown list
+
+        }
+    }
+
+    protected void drpRestaurants_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+        //show the selected restaurant data as specified in the lab requirements 
+    }
+   
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        //Save the changed restaurant restaurant data back to the XML file. 
+    }
+
+
+}
